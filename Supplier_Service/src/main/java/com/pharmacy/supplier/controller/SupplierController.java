fdf1eb9a181cf8152f3d8897e77783bc4f72638f
akankshamahankar11 <akanksha.pramod-mahankar@capgemini.com>
2025-07-03 12:55:18 +0530

    Update SupplierController.java

--- Supplier_Service/src/main/java/com/pharmacy/supplier/controller/SupplierController.java
+++ Supplier_Service/src/main/java/com/pharmacy/supplier/controller/SupplierController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import com.pharmacy.supplier.entity.Supplier;
 import com.pharmacy.supplier.service.SupplierService;
 
+import jakarta.validation.Valid;
+
 import java.util.List;
 
 @RestController
@@ -26,13 +28,23 @@ public class SupplierController {
         return ResponseEntity.ok(supplierService.getSupplierById(id));
     }
 
-    @PostMapping
-    public Supplier createSupplier(@RequestBody Supplier supplier) {
-        return supplierService.createSupplier(supplier);
-    }
+    
+//    @PostMapping
+//    public Supplier createSupplier(@Valid @RequestBody Supplier supplier) {
+//        return supplierService.createSupplier(supplier);
+//    }
+    
+	
+	@PostMapping
+	public ResponseEntity<String> createSupplier(@Valid @RequestBody Supplier supplier) {
+		// Validation is triggered here
+		supplierService.createSupplier(supplier);
+	return ResponseEntity.ok("Saved");
+	}
+
 
     @PutMapping("/{id}")
-    public ResponseEntity<Supplier> updateSupplier(@PathVariable Long id, @RequestBody Supplier supplier) {
+    public ResponseEntity<Supplier> updateSupplier(@PathVariable Long id,@Valid @RequestBody Supplier supplier) {
         return ResponseEntity.ok(supplierService.updateSupplier(id, supplier));
     }
 
